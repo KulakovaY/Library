da9384905c1c658c043e8fe4ef2fa47128f7ac75
KulakovaY <144993961+KulakovaY@users.noreply.github.com>
2026-04-24 19:37:06 +0300

    feat: console writer done

--- src/main/java/ru/kulakova/writers/ConsoleWriter.java
+++ src/main/java/ru/kulakova/writers/ConsoleWriter.java
@@ -1,4 +1,77 @@
 package ru.kulakova.writers;
 
-public class ConsoleWriter {
+import ru.kulakova.entities.Book;
+import ru.kulakova.entities.LibraryStats;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ConsoleWriter implements Writable{
+
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    @Override
+    public void writeSuccess(String message) {
+        System.out.println(ANSI_GREEN + message + ANSI_RESET);
+    }
+
+    @Override
+    public void writeNotification(String message) {
+        System.out.println(ANSI_YELLOW + message + ANSI_RESET);
+    }
+
+
+    @Override
+    public void writeError(String message) {
+        System.out.println(ANSI_RED + message + ANSI_RESET);
+    }
+
+    @Override
+    public void writeUUID(UUID id) {
+        System.out.println("ID: " + id);
+    }
+
+    @Override
+    public void writeBook(Book book) {
+        System.out.printf(" %s — %s (%d) [ID: %s]%n",
+                book.getTitle(),
+                book.getAuthor(),
+                book.getYear(),
+                book.getId());
+    }
+
+    @Override
+    public void writeBookList(List<Book> books) {
+        System.out.println("Книги:");
+        for (int i = 0; i < books.size(); i++) {
+            Book b = books.get(i);
+            System.out.printf("%d. %s — %s (%d)%n",
+                    i + 1, b.getTitle(), b.getAuthor(), b.getYear());
+        }
+    }
+
+    @Override
+    public void writeStats(LibraryStats stats) {
+        System.out.println("Статистика:");
+        System.out.println("Всего книг: " + stats.totalCount());
+
+        stats.oldest().ifPresent(b ->
+                System.out.println("Самая старая: " + b.getTitle() + " (" + b.getYear() + ")"));
+        stats.newest().ifPresent(b ->
+                System.out.println("Самая новая: " + b.getTitle() + " (" + b.getYear() + ")"));
+
+        System.out.println("\nТоп авторов:");
+        if (stats.topAuthors().isEmpty()) {
+            writeNotification("  — Нет данных");
+        } else {
+            int rank = 1;
+            for (var entry : stats.topAuthors().entrySet()) {
+                System.out.printf("  %d. %s: %d книг%n",
+                        rank++, entry.getKey(), entry.getValue());
+            }
+        }
+    }
 }
--- src/main/java/ru/kulakova/writers/Writable.java
+++ src/main/java/ru/kulakova/writers/Writable.java
@@ -1,4 +1,20 @@
 package ru.kulakova.writers;
 
+import ru.kulakova.entities.*;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface Writable {
+
+    void writeSuccess(String message);
+    void writeNotification(String message);
+    void writeError(String message);
+
+    void writeUUID(UUID id);
+
+    void writeBook(Book book);
+    void writeBookList(List<Book> books);
+
+    void writeStats(LibraryStats stats);
 }
